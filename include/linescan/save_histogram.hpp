40f315477e2868580852e4e0c47bbd278ad442b2
Benjamin Buch <benni.buch@gmail.com>
2015-10-15 16:34:07 +0200

    histogram calculation

---
+++ include/linescan/save_histogram.hpp
@@ -0,0 +1,24 @@
+//-----------------------------------------------------------------------------
+// Copyright (c) 2015 Benjamin Buch
+//
+// https://github.com/bebuch/linescan
+//
+// Distributed under the Boost Software License, Version 1.0. (See accompanying
+// file LICENSE_1_0.txt or copy at https://www.boost.org/LICENSE_1_0.txt)
+//-----------------------------------------------------------------------------
+#ifndef _linescan__save_histogram__hpp_INCLUDED_
+#define _linescan__save_histogram__hpp_INCLUDED_
+
+#include "bitmap.hpp"
+
+
+namespace linescan{
+
+
+	void save_histogram(bitmap< std::uint8_t > const& image);
+
+
+}
+
+
+#endif
