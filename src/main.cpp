4cf3534ac54aeb50830fb4b3e01d62a0588596f4
Benjamin Buch <benni.buch@gmail.com>
2015-10-19 17:53:22 +0200

    get a single image

--- src/main.cpp
+++ src/main.cpp
@@ -20,7 +20,20 @@
 #include <iomanip>
 
 
+void save(linescan::bitmap< std::uint8_t > const& image, std::string const& name){
+	png::image< png::gray_pixel > output(image.width(), image.height());
+	for(std::size_t y = 0; y < image.height(); ++y){
+		for(std::size_t x = 0; x < image.width(); ++x){
+			output[y][x] = image(x, y);
+		}
+	}
+	output.write(name);
+}
+
+
 int main()try{
+	using namespace std::literals;
+
 	linescan::control_F9S_MCL3 mcl3("/dev/ttyUSB0");
 
 	linescan::camera cam(0);
@@ -37,19 +50,47 @@ int main()try{
 			mcl3.activate_joystick();
 		}else if(command == "move"){
 			mcl3.move_to(100000, 100000, 100000);
+		}else if(command == "image"){
+			cam.set_exposure(cam.exposure_in_ms_max());
+
+			std::this_thread::sleep_for(100ms);
+
+			auto image = cam.image();
+
+			save(image, "01_image.png");
+
+			for(std::size_t y = 0; y < image.height(); ++y){
+				for(std::size_t x = 0; x < image.width(); ++x){
+					image(x, y) = image(x, y) < 255 ? 0 : 255;
+				}
+			}
+
+			save(image, "02_binary.png");
+
+			auto result = image;
+			for(std::size_t y = 2; y < image.height() - 2; ++y){
+				for(std::size_t x = 2; x < image.width() - 2; ++x){
+					for(std::size_t b = 0; b < 5; ++b){
+						for(std::size_t a = 0; a < 5; ++a){
+							if(!image(x + a - 2, y + b - 2)) continue;
+							result(x, y) = 255;
+						}
+					}
+				}
+			}
+
+			save(result, "03_erode.png");
 		}else if(command == "measure"){
 			mcl3.move_relative(0, 0, 1000);
 
 			auto diff =
-				(cam.exposure_in_ms_max() - cam.exposure_in_ms_min()) / 10;
+				(cam.exposure_in_ms_max() - cam.exposure_in_ms_min()) / 9;
 
 // 			double from = 0;
 // 			double to = cam.width() * cam.pixel_size_in_um();
 			std::vector< std::pair< double, double > > lines;
 			for(std::size_t i = 0; i < 20; ++i){
 				for(std::size_t n = 5; n < 10; ++n){
-					using namespace std::literals;
-
 					cam.set_exposure(cam.exposure_in_ms_min() + n * diff);
 
 					std::this_thread::sleep_for(100ms);
@@ -57,18 +98,12 @@ int main()try{
 					auto image = cam.image();
 
 					{
-						png::image< png::gray_pixel > output(image.width(), image.height());
-						for(std::size_t y = 0; y < image.height(); ++y){
-							for(std::size_t x = 0; x < image.width(); ++x){
-								output[y][x] = image(x, y);
-							}
-						}
 						std::ostringstream os;
 						os
 							<< "img" << std::setfill('0')
 							<< std::setw(4) << i << "_"
 							<< std::setw(4) << n << ".png";
-						output.write(os.str());
+						save(image, os.str());
 					}
 
 					auto pixel_line = calc_line(image);
