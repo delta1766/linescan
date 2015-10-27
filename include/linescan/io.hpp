aeb6b93612abfb634084037c1aeceb08b2ad7f71
Benjamin Buch <benni.buch@gmail.com>
2015-10-27 15:34:43 +0100

    fix include protection

--- include/linescan/io.hpp
+++ include/linescan/io.hpp
@@ -6,8 +6,8 @@
 // Distributed under the Boost Software License, Version 1.0. (See accompanying
 // file LICENSE_1_0.txt or copy at https://www.boost.org/LICENSE_1_0.txt)
 //-----------------------------------------------------------------------------
-#ifndef _disposer_module__io__hpp_INCLUDED_
-#define _disposer_module__io__hpp_INCLUDED_
+#ifndef _linescan__io__hpp_INCLUDED_
+#define _linescan__io__hpp_INCLUDED_
 
 #include <iostream>
 
