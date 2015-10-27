6746718819a7ee3e094d3c494e030319cb264b61
Benjamin Buch <benni.buch@gmail.com>
2015-10-27 17:10:40 +0100

    comment out hardware connections and start to add calibration implementation

--- src/main.cpp
+++ src/main.cpp
@@ -13,6 +13,8 @@
 #include <linescan/binarize.hpp>
 #include <linescan/erode.hpp>
 #include <linescan/gauss.hpp>
+#include <linescan/edge.hpp>
+#include <linescan/normelize_to_uint8.hpp>
 
 #include <boost/type_index.hpp>
 
@@ -22,6 +24,19 @@
 #include <sstream>
 #include <fstream>
 #include <iomanip>
+
+
+void load(linescan::bitmap< std::uint8_t >& image, std::string const& name){
+	std::cout << "read " << name << std::endl;
+	png::image< png::gray_pixel > output;
+	output.read(name);
+	image.resize(output.get_width(), output.get_height());
+	for(std::size_t y = 0; y < output.get_height(); ++y){
+		for(std::size_t x = 0; x < output.get_width(); ++x){
+			image(x, y) = output[y][x];
+		}
+	}
+}
 
 
 void save(linescan::bitmap< std::uint8_t > const& image, std::string const& name){
@@ -46,6 +61,10 @@ void save(linescan::bitmap< bool > const& image, std::string const& name){
 	output.write(name);
 }
 
+void save(linescan::bitmap< std::int32_t > const& image, std::string const& name){
+	save(linescan::normelize_to_uint8(image), name);
+}
+
 void save(std::vector< float > const& line, std::size_t height, std::string const& name){
 	std::cout << "write " << name << std::endl;
 	png::image< png::gray_pixel > output(line.size(), height);
@@ -67,203 +86,226 @@ void save(std::vector< float > const& line, std::size_t height, std::string cons
 int main()try{
 	using namespace std::literals;
 
-	linescan::control_F9S_MCL3 mcl3("/dev/ttyUSB0");
-
-	linescan::camera cam(0);
+// 	linescan::control_F9S_MCL3 mcl3("/dev/ttyUSB0");
+// 
+// 	linescan::camera cam(0);
 
 	std::string command;
 	while(getline(std::cin, command)){
 		if(command.empty()) break;
 
-		if(command == "calib"){
-			mcl3.calibrate();
-		}else if(command == "stop"){
-			mcl3.stop();
-		}else if(command == "joy"){
-			mcl3.activate_joystick();
-		}else if(command == "move"){
-			mcl3.move_to(100000, 100000, 100000);
-		}else if(command == "image"){
-			cam.set_exposure(cam.exposure_in_ms_max());
-
-			std::this_thread::sleep_for(100ms);
-
-			auto image = cam.image();
-
-			save(image, "01_image.png");
-
-			image = linescan::gauss< 5 >(image, 0.7);
-
-			save(image, "02_gauss.png");
+// 		if(command == "calib"){
+// 			mcl3.calibrate();
+// 		}else if(command == "stop"){
+// 			mcl3.stop();
+// 		}else if(command == "joy"){
+// 			mcl3.activate_joystick();
+// 		}else if(command == "move"){
+// 			mcl3.move_to(100000, 100000, 100000);
+// 		}else if(command == "image"){
+// 			cam.set_exposure(cam.exposure_in_ms_max());
+// 
+// 			std::this_thread::sleep_for(100ms);
+// 
+// 			auto image = cam.image();
+// 
+// 			save(image, "01_image.png");
+// 
+// 			image = linescan::gauss< 5 >(image, 0.7);
+// 
+// 			save(image, "02_gauss.png");
+// 
+// 			auto binary = linescan::binarize(image, std::uint8_t(255));
+// 
+// 			save(binary, "03_binary.png");
+// 
+// 			binary = linescan::erode(binary, 5);
+// 
+// 			save(binary, "04_erode.png");
+// 
+// 			auto line = linescan::calc_line(binary);
+// 
+// 			save(line, binary.height(), "05_line.png");
+// 		}else if(command == "measure"){
+// 			mcl3.move_relative(0, 0, 1000);
+// 
+// 			auto diff =
+// 				(cam.exposure_in_ms_max() - cam.exposure_in_ms_min()) / 9;
+// 
+// // 			double from = 0;
+// // 			double to = cam.width() * cam.pixel_size_in_um();
+// 			std::vector< std::pair< double, double > > lines;
+// 			for(std::size_t i = 0; i < 20; ++i){
+// 				for(std::size_t n = 5; n < 10; ++n){
+// 					cam.set_exposure(cam.exposure_in_ms_min() + n * diff);
+// 
+// 					std::this_thread::sleep_for(100ms);
+// 
+// 					auto image = cam.image();
+// 
+// 					{
+// 						std::ostringstream os;
+// 						os
+// 							<< "img" << std::setfill('0')
+// 							<< std::setw(4) << i << "_"
+// 							<< std::setw(4) << n << ".png";
+// 						save(image, os.str());
+// 					}
+// 
+// 
+// 					auto pixel_line = calc_line(image);
+// 
+// 					std::ostringstream os;
+// 					os
+// 						<< "line" << std::setfill('0')
+// 						<< std::setw(4) << i << "_"
+// 						<< std::setw(4) << n << ".png";
+// 					save(pixel_line, image.height(), os.str());
+// 
+// 
+// 					std::vector< linescan::point< double > > line;
+// 					for(std::size_t i = 0; i < pixel_line.size(); ++i){
+// 						if(pixel_line[i] == 0) continue;
+// 
+// 						line.emplace_back(
+// 							i * cam.pixel_size_in_um(),
+// 							pixel_line[i] * cam.pixel_size_in_um()
+// 						);
+// 					}
+// 
+// 					auto f = linescan::fit_linear_function< double >(
+// 						line.begin(),
+// 						line.end()
+// 					);
+// 
+// 	// 				lines.emplace_back(f(from), f(to));
+// 
+// 					std::cout << "(0; " << f(0) << ") - (100; " << f(100) << ")"
+// 						<< std::endl;
+// 				}
+// 
+// 				mcl3.move_relative(0, 0, -100);
+// 			}
+// 
+// // 			double diff_100um = 0;
+// // 			for(std::size_t i = 0; i < lines.size() - 1; ++i){
+// // 				diff_100um +=
+// // 					lines[i + 1].first - lines[i].first + 
+// // 					lines[i + 1].second - lines[i].second;
+// // 			}
+// // 			diff_100um /= (lines.size() - 1) * 2;
+// 
+// 			mcl3.move_relative(0, 0, 1000);
+// 		}else if(command == "line"){
+// 			cam.set_exposure(cam.exposure_in_ms_max());
+// 			std::this_thread::sleep_for(100ms);
+// 
+// 			std::ofstream raw("raw.txt");
+// 			std::ofstream os("line.txt");
+// 
+// 			mcl3.move_relative(0, 0, 10000);
+// 			for(std::size_t i = 0; i < 500; ++i){
+// 				mcl3.move_relative(0, 300, 0);
+// 
+// 				std::vector< std::pair< std::size_t, std::size_t > > lines;
+// 				for(std::size_t n = 0; n < 60; ++n){
+// 					auto image = cam.image();
+// 					auto binary = linescan::binarize(image, std::uint8_t(255));
+// 					binary = linescan::erode(binary, 5);
+// 					auto line = linescan::calc_line(binary);
+// 
+// 					// add longest contiguous stretch
+// 					std::size_t from = 0;
+// 					std::size_t to = 0;
+// 
+// 					std::size_t max_length = 0;
+// 					std::size_t start = 0;
+// 					for(std::size_t i = 0; i < line.size(); ++i){
+// 						if(!line[i]){
+// 							if(start == 0) continue;
+// 
+// 							auto length = i - start;
+// 							if(length > max_length){
+// 								max_length = length;
+// 
+// 								from = start;
+// 								to = i;
+// 							}
+// 
+// 							start = 0;
+// 						}else{
+// 							if(start != 0) continue;
+// 
+// 							start = i;
+// 						}
+// 					}
+// 
+// 					lines.emplace_back(from, to);
+// 
+// 					mcl3.move_relative(0, -10, 0);
+// 				}
+// 
+// 				for(auto line: lines){
+// 					raw << line.first << '\t' << line.second << std::endl;
+// 				}
+// 				raw << std::endl;
+// 
+// 				// sort by start
+// 				std::sort(lines.begin(), lines.end(), [](auto a, auto b){
+// 					return a.first < b.first;
+// 				});
+// 				lines.erase(lines.begin(), lines.begin() + 10);
+// 				lines.erase(lines.end() - 10, lines.end());
+// 
+// 				// sort by end
+// 				std::sort(lines.begin(), lines.end(), [](auto a, auto b){
+// 					return a.second < b.second;
+// 				});
+// 				lines.erase(lines.begin(), lines.begin() + 10);
+// 				lines.erase(lines.end() - 10, lines.end());
+// 
+// 				// calc average of the rest
+// 				std::pair< double, double > length(0, 0);
+// 				for(auto line: lines){
+// 					length.first += line.first;
+// 					length.second += line.second;
+// 				}
+// 				length.first /= lines.size();
+// 				length.second /= lines.size();
+// 
+// 				os << length.first << '\t' << length.second << std::endl;
+// 
+// 				mcl3.move_relative(0, 300, 0);
+// 
+// 				mcl3.move_relative(0, 0, -200);
+// 			}
+// 
+// 			mcl3.move_relative(0, 0, 10000);
+// 		}else if(command == "end"){
+// 			mcl3.move_to_end();
+		/*}else */if(command == "load"){
+			linescan::bitmap< std::uint8_t > image;
+			load(image, "scene.png");
 
 			auto binary = linescan::binarize(image, std::uint8_t(255));
 
 			save(binary, "03_binary.png");
 
-			binary = linescan::erode(binary, 5);
+			binary = linescan::erode(binary, 3);
 
 			save(binary, "04_erode.png");
 
 			auto line = linescan::calc_line(binary);
 
 			save(line, binary.height(), "05_line.png");
-		}else if(command == "measure"){
-			mcl3.move_relative(0, 0, 1000);
 
-			auto diff =
-				(cam.exposure_in_ms_max() - cam.exposure_in_ms_min()) / 9;
+			load(image, "scene_raw.png");
 
-// 			double from = 0;
-// 			double to = cam.width() * cam.pixel_size_in_um();
-			std::vector< std::pair< double, double > > lines;
-			for(std::size_t i = 0; i < 20; ++i){
-				for(std::size_t n = 5; n < 10; ++n){
-					cam.set_exposure(cam.exposure_in_ms_min() + n * diff);
+			linescan::normelize_to_uint8(image);
 
-					std::this_thread::sleep_for(100ms);
+			auto edge = linescan::edge_amplitude(image);
 
-					auto image = cam.image();
-
-					{
-						std::ostringstream os;
-						os
-							<< "img" << std::setfill('0')
-							<< std::setw(4) << i << "_"
-							<< std::setw(4) << n << ".png";
-						save(image, os.str());
-					}
-
-
-					auto pixel_line = calc_line(image);
-
-					std::ostringstream os;
-					os
-						<< "line" << std::setfill('0')
-						<< std::setw(4) << i << "_"
-						<< std::setw(4) << n << ".png";
-					save(pixel_line, image.height(), os.str());
-
-
-					std::vector< linescan::point< double > > line;
-					for(std::size_t i = 0; i < pixel_line.size(); ++i){
-						if(pixel_line[i] == 0) continue;
-
-						line.emplace_back(
-							i * cam.pixel_size_in_um(),
-							pixel_line[i] * cam.pixel_size_in_um()
-						);
-					}
-
-					auto f = linescan::fit_linear_function< double >(
-						line.begin(),
-						line.end()
-					);
-
-	// 				lines.emplace_back(f(from), f(to));
-
-					std::cout << "(0; " << f(0) << ") - (100; " << f(100) << ")"
-						<< std::endl;
-				}
-
-				mcl3.move_relative(0, 0, -100);
-			}
-
-// 			double diff_100um = 0;
-// 			for(std::size_t i = 0; i < lines.size() - 1; ++i){
-// 				diff_100um +=
-// 					lines[i + 1].first - lines[i].first + 
-// 					lines[i + 1].second - lines[i].second;
-// 			}
-// 			diff_100um /= (lines.size() - 1) * 2;
-
-			mcl3.move_relative(0, 0, 1000);
-		}else if(command == "line"){
-			cam.set_exposure(cam.exposure_in_ms_max());
-			std::this_thread::sleep_for(100ms);
-
-			std::ofstream raw("raw.txt");
-			std::ofstream os("line.txt");
-
-			mcl3.move_relative(0, 0, 10000);
-			for(std::size_t i = 0; i < 500; ++i){
-				mcl3.move_relative(0, 300, 0);
-
-				std::vector< std::pair< std::size_t, std::size_t > > lines;
-				for(std::size_t n = 0; n < 60; ++n){
-					auto image = cam.image();
-					auto binary = linescan::binarize(image, std::uint8_t(255));
-					binary = linescan::erode(binary, 5);
-					auto line = linescan::calc_line(binary);
-
-					// add longest contiguous stretch
-					std::size_t from = 0;
-					std::size_t to = 0;
-
-					std::size_t max_length = 0;
-					std::size_t start = 0;
-					for(std::size_t i = 0; i < line.size(); ++i){
-						if(!line[i]){
-							if(start == 0) continue;
-
-							auto length = i - start;
-							if(length > max_length){
-								max_length = length;
-
-								from = start;
-								to = i;
-							}
-
-							start = 0;
-						}else{
-							if(start != 0) continue;
-
-							start = i;
-						}
-					}
-
-					lines.emplace_back(from, to);
-
-					mcl3.move_relative(0, -10, 0);
-				}
-
-				for(auto line: lines){
-					raw << line.first << '\t' << line.second << std::endl;
-				}
-				raw << std::endl;
-
-				// sort by start
-				std::sort(lines.begin(), lines.end(), [](auto a, auto b){
-					return a.first < b.first;
-				});
-				lines.erase(lines.begin(), lines.begin() + 10);
-				lines.erase(lines.end() - 10, lines.end());
-
-				// sort by end
-				std::sort(lines.begin(), lines.end(), [](auto a, auto b){
-					return a.second < b.second;
-				});
-				lines.erase(lines.begin(), lines.begin() + 10);
-				lines.erase(lines.end() - 10, lines.end());
-
-				// calc average of the rest
-				std::pair< double, double > length(0, 0);
-				for(auto line: lines){
-					length.first += line.first;
-					length.second += line.second;
-				}
-				length.first /= lines.size();
-				length.second /= lines.size();
-
-				os << length.first << '\t' << length.second << std::endl;
-
-				mcl3.move_relative(0, 300, 0);
-
-				mcl3.move_relative(0, 0, -200);
-			}
-
-			mcl3.move_relative(0, 0, 10000);
-		}else if(command == "end"){
-			mcl3.move_to_end();
+			save(edge, "06_edge.png");
 		}else{
 			std::cout << "Unknown input" << std::endl;
 		}
