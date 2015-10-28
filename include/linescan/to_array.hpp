38aaff1998b3d37a61f21a773807f83efdab7610
Benjamin Buch <benni.buch@gmail.com>
2015-10-28 21:18:16 +0100

    add move semantic version

--- include/linescan/to_array.hpp
+++ include/linescan/to_array.hpp
@@ -21,6 +21,11 @@ namespace linescan{
 
 
 		template < typename T, std::size_t N, std::size_t ... I >
+		constexpr auto to_array(T(&&arr)[N], std::index_sequence< I ... >){
+			return std::array< std::remove_cv_t< T >, N >{{ std::move(arr[I]) ... }};
+		}
+
+		template < typename T, std::size_t N, std::size_t ... I >
 		constexpr auto to_array(T(&arr)[N], std::index_sequence< I ... >){
 			return std::array< std::remove_cv_t< T >, N >{{ arr[I] ... }};
 		}
@@ -28,6 +33,11 @@ namespace linescan{
 
 	}
 
+
+	template < typename T, std::size_t N >
+	constexpr auto to_array(T(&&arr)[N]){
+		return detail::to_array(std::move(arr), std::make_index_sequence< N >());
+	}
 
 	template < typename T, std::size_t N >
 	constexpr auto to_array(T(&arr)[N]){
