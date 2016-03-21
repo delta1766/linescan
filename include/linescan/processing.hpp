46a0435c7b84de34cb65173d09468416eac1e44f
Benjamin Buch <benni.buch@gmail.com>
2016-03-21 15:44:55 +0100

    move erode and gauss into processing.[ch]pp

---
+++ include/linescan/processing.hpp
@@ -0,0 +1,35 @@
+//-----------------------------------------------------------------------------
+// Copyright (c) 2015-2016 Benjamin Buch
+//
+// https://github.com/bebuch/linescan
+//
+// Distributed under the Boost Software License, Version 1.0. (See accompanying
+// file LICENSE_1_0.txt or copy at https://www.boost.org/LICENSE_1_0.txt)
+//-----------------------------------------------------------------------------
+#ifndef _linescan__processing__hpp_INCLUDED_
+#define _linescan__processing__hpp_INCLUDED_
+
+#include <mitrax/transform.hpp>
+#include <mitrax/pass_in.hpp>
+
+
+namespace linescan{
+
+
+	mitrax::raw_bitmap< bool > erode(
+		mitrax::raw_bitmap< bool > const& image,
+		std::size_t size,
+		bool border_value = false
+	);
+
+	mitrax::raw_bitmap< std::uint8_t > gauss(
+		mitrax::raw_bitmap< std::uint8_t > const& image,
+		std::size_t size,
+		float variance = 0.7f
+	);
+
+
+}
+
+
+#endif
---
+++ src/processing.cpp
@@ -0,0 +1,74 @@
+//-----------------------------------------------------------------------------
+// Copyright (c) 2016 Benjamin Buch
+//
+// https://github.com/bebuch/linescan
+//
+// Distributed under the Boost Software License, Version 1.0. (See accompanying
+// file LICENSE_1_0.txt or copy at https://www.boost.org/LICENSE_1_0.txt)
+//-----------------------------------------------------------------------------
+#include <linescan/processing.hpp>
+
+#include <mitrax/convolution.hpp>
+
+#include <cmath>
+
+
+namespace linescan{
+
+
+	mitrax::raw_bitmap< bool > erode(
+		mitrax::raw_bitmap< bool > const& image,
+		std::size_t size,
+		bool border_value
+	){
+		return mitrax::pass_in(image.dims(),
+			mitrax::transform_per_view([](auto const& m){
+				bool result = false;
+
+				for(std::size_t y = 0; y < m.rows(); ++y){
+					for(std::size_t x = 0; x < m.cols(); ++x){
+						if(!m(x, y)) continue;
+						result = true;
+					}
+				}
+
+				return result;
+			}, mitrax::cols(size), mitrax::rows(size), image),
+			border_value
+		);
+	}
+
+	mitrax::raw_bitmap< std::uint8_t > gauss(
+		mitrax::raw_bitmap< std::uint8_t > const& image,
+		std::size_t size,
+		float variance
+	){
+		auto vc = mitrax::make_col_vector< float >(mitrax::rows(size));
+		auto vr = mitrax::make_row_vector< float >(mitrax::cols(size));
+
+		float sum = 0;
+		for(std::size_t i = 0; i < size; ++i){
+			auto x = i - size / 2.f + .5f;
+
+			auto value =
+				std::exp(- ((x * x) / (2 * variance * variance))) /
+				(variance * std::sqrt(2 * 3.14159f));
+
+			vc[i] = value;
+
+			sum += value;
+		}
+
+		for(std::size_t i = 0; i < size; ++i){
+			vc[i] /= sum;
+			vr[i] = vc[i];
+		}
+
+		return transform([](auto v){
+			return static_cast< std::uint8_t >(v);
+		}, convolution(image, vc, vr));
+	}
+
+
+
+}
