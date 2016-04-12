eeea36e74facc84567674f77d8e6013afc6b5186
Benjamin Buch <benni.buch@gmail.com>
2016-04-12 18:16:55 +0200

    Add translation files

---
+++ lang/linescan_de.ts
@@ -0,0 +1,387 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="de" sourcelanguage="en">
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="../include/linescan/exception_catcher.hpp" line="33"/>
+        <location filename="../src/main.cpp" line="37"/>
+        <location filename="../src/widget_calib.cpp" line="158"/>
+        <location filename="../src/widget_measure.cpp" line="264"/>
+        <source>Error</source>
+        <translation>Fehler</translation>
+    </message>
+    <message>
+        <location filename="../include/linescan/exception_catcher.hpp" line="49"/>
+        <location filename="../src/main.cpp" line="48"/>
+        <source>Fatal Error</source>
+        <translation>Schwerer Fehler</translation>
+    </message>
+    <message>
+        <location filename="../src/draw.cpp" line="93"/>
+        <source>no line</source>
+        <translation>keine Linie</translation>
+    </message>
+    <message>
+        <location filename="../src/draw.cpp" line="159"/>
+        <source>not two circles found</source>
+        <translation>keine zwei Kreise gefunden</translation>
+    </message>
+    <message>
+        <location filename="../src/draw.cpp" line="166"/>
+        <source>can not make linear function</source>
+        <translation>lineare Funktion kann nicht erstellt werden</translation>
+    </message>
+</context>
+<context>
+    <name>linescan::widget_calib</name>
+    <message>
+        <location filename="../src/widget_calib.cpp" line="159"/>
+        <source>%1 errors back-to-back, cancle calibration?</source>
+        <translation>%1 Fehler nacheinander, Kalibrierung abbrechen?</translation>
+    </message>
+    <message>
+        <location filename="../src/widget_calib.cpp" line="270"/>
+        <location filename="../src/widget_calib.cpp" line="276"/>
+        <source>Z = %L1 mm</source>
+        <translation>Z = %L1 mm</translation>
+    </message>
+    <message>
+        <location filename="../src/widget_calib.cpp" line="328"/>
+        <source>Step %1 from 3</source>
+        <translation>Schritt %1 von 3</translation>
+    </message>
+    <message>
+        <location filename="../src/widget_calib.cpp" line="333"/>
+        <source>Laser aligned</source>
+        <translation>Laser ausgerichtet</translation>
+    </message>
+    <message>
+        <location filename="../src/widget_calib.cpp" line="347"/>
+        <source>Target aligned</source>
+        <translation>Kreise ausgerichtet</translation>
+    </message>
+    <message>
+        <location filename="../src/widget_calib.cpp" line="360"/>
+        <source>Stop</source>
+        <translation>Stopp</translation>
+    </message>
+    <message>
+        <location filename="../src/widget_calib.cpp" line="366"/>
+        <source>complete</source>
+        <translation>Fertig</translation>
+    </message>
+    <message>
+        <location filename="../src/widget_calib.cpp" line="367"/>
+        <source>Reset</source>
+        <translation>Neustart</translation>
+    </message>
+</context>
+<context>
+    <name>linescan::widget_main_window</name>
+    <message>
+        <location filename="../src/widget_main_window.cpp" line="39"/>
+        <source>Measure</source>
+        <translation>Messung</translation>
+    </message>
+    <message>
+        <location filename="../src/widget_main_window.cpp" line="40"/>
+        <source>Calibration</source>
+        <translation>Kalibrierung</translation>
+    </message>
+    <message>
+        <location filename="../src/widget_main_window.cpp" line="60"/>
+        <source>Show</source>
+        <translation>Anzeige</translation>
+    </message>
+</context>
+<context>
+    <name>linescan::widget_measure</name>
+    <message>
+        <location filename="../src/widget_measure.cpp" line="26"/>
+        <source>primary direction (Y)</source>
+        <translation>Primäre Richtung (Y)</translation>
+    </message>
+    <message>
+        <location filename="../src/widget_measure.cpp" line="27"/>
+        <location filename="../src/widget_measure.cpp" line="31"/>
+        <source>From: </source>
+        <translation>Von:</translation>
+    </message>
+    <message>
+        <location filename="../src/widget_measure.cpp" line="28"/>
+        <location filename="../src/widget_measure.cpp" line="32"/>
+        <source>To: </source>
+        <translation>Nach:</translation>
+    </message>
+    <message>
+        <location filename="../src/widget_measure.cpp" line="29"/>
+        <location filename="../src/widget_measure.cpp" line="33"/>
+        <source>Step: </source>
+        <translation>Schritt:</translation>
+    </message>
+    <message>
+        <location filename="../src/widget_measure.cpp" line="30"/>
+        <source>secondary direction (X)</source>
+        <translation>Sekundäre Richtung (X)</translation>
+    </message>
+    <message>
+        <location filename="../src/widget_measure.cpp" line="34"/>
+        <location filename="../src/widget_measure.cpp" line="325"/>
+        <source>Start measurement</source>
+        <translation>Messung starten</translation>
+    </message>
+    <message>
+        <location filename="../src/widget_measure.cpp" line="35"/>
+        <source>Save image</source>
+        <translation>Bild speichern</translation>
+    </message>
+    <message>
+        <location filename="../src/widget_measure.cpp" line="76"/>
+        <location filename="../src/widget_measure.cpp" line="77"/>
+        <location filename="../src/widget_measure.cpp" line="78"/>
+        <location filename="../src/widget_measure.cpp" line="80"/>
+        <location filename="../src/widget_measure.cpp" line="81"/>
+        <location filename="../src/widget_measure.cpp" line="82"/>
+        <source> mm</source>
+        <translation> mm</translation>
+    </message>
+    <message>
+        <location filename="../src/widget_measure.cpp" line="136"/>
+        <source>Save image &apos;%1&apos;.</source>
+        <translation>Bild speichern &apos;%1&apos;.</translation>
+    </message>
+    <message>
+        <location filename="../src/widget_measure.cpp" line="265"/>
+        <source>%1 errors back-to-back, stop measure?</source>
+        <translation>%1 Fehler nacheinander, Messung abbrechen?</translation>
+    </message>
+    <message>
+        <location filename="../src/widget_measure.cpp" line="299"/>
+        <source>Stop measurement</source>
+        <translation>Messung beenden</translation>
+    </message>
+    <message>
+        <location filename="../src/widget_measure.cpp" line="332"/>
+        <source>Save measurement &apos;%1&apos;.</source>
+        <translation>Messung speichern &apos;%1&apos;.</translation>
+    </message>
+</context>
+<context>
+    <name>linescan::widgetdock_calc_laser_line</name>
+    <message>
+        <location filename="../src/widgetdock_calc_laser_line.cpp" line="17"/>
+        <source>Laser line</source>
+        <translation>Laserlinie</translation>
+    </message>
+    <message>
+        <location filename="../src/widgetdock_calc_laser_line.cpp" line="18"/>
+        <source>Calculation method</source>
+        <translation>Kalibriermethode</translation>
+    </message>
+    <message>
+        <location filename="../src/widgetdock_calc_laser_line.cpp" line="19"/>
+        <source>Threshold</source>
+        <translation>Schwellwert</translation>
+    </message>
+    <message>
+        <location filename="../src/widgetdock_calc_laser_line.cpp" line="20"/>
+        <source>Sum</source>
+        <translation>Summe</translation>
+    </message>
+    <message>
+        <location filename="../src/widgetdock_calc_laser_line.cpp" line="21"/>
+        <location filename="../src/widgetdock_calc_laser_line.cpp" line="29"/>
+        <source>Settings</source>
+        <translation>Einstellungen</translation>
+    </message>
+    <message>
+        <location filename="../src/widgetdock_calc_laser_line.cpp" line="22"/>
+        <location filename="../src/widgetdock_calc_laser_line.cpp" line="30"/>
+        <source>Show original</source>
+        <translation>Zeige Original</translation>
+    </message>
+    <message>
+        <location filename="../src/widgetdock_calc_laser_line.cpp" line="23"/>
+        <source>Show binarized</source>
+        <translation>Zeige Binärbild</translation>
+    </message>
+    <message>
+        <location filename="../src/widgetdock_calc_laser_line.cpp" line="24"/>
+        <source>Threshold: </source>
+        <translation>Schwellwert: </translation>
+    </message>
+    <message>
+        <location filename="../src/widgetdock_calc_laser_line.cpp" line="25"/>
+        <source>Show eroded</source>
+        <translation>Zeige erodiertes Bild</translation>
+    </message>
+    <message>
+        <location filename="../src/widgetdock_calc_laser_line.cpp" line="26"/>
+        <source>Erode: </source>
+        <translation>Erodierung: </translation>
+    </message>
+    <message>
+        <location filename="../src/widgetdock_calc_laser_line.cpp" line="27"/>
+        <location filename="../src/widgetdock_calc_laser_line.cpp" line="33"/>
+        <source>Show line</source>
+        <translation>Zeige Linienbild</translation>
+    </message>
+    <message>
+        <location filename="../src/widgetdock_calc_laser_line.cpp" line="28"/>
+        <location filename="../src/widgetdock_calc_laser_line.cpp" line="34"/>
+        <source>Subpixel: </source>
+        <translation>Subpixel: </translation>
+    </message>
+    <message>
+        <location filename="../src/widgetdock_calc_laser_line.cpp" line="31"/>
+        <source>Minimal value: </source>
+        <translation>Minimalwert: </translation>
+    </message>
+    <message>
+        <location filename="../src/widgetdock_calc_laser_line.cpp" line="32"/>
+        <source>Minimal sum: </source>
+        <translation>Minimale Summe: </translation>
+    </message>
+</context>
+<context>
+    <name>linescan::widgetdock_camera</name>
+    <message>
+        <location filename="../src/widgetdock_camera.cpp" line="50"/>
+        <source>Camera settings</source>
+        <translation>Kameraeinstellungen</translation>
+    </message>
+    <message>
+        <location filename="../src/widgetdock_camera.cpp" line="52"/>
+        <source>Pixelclock</source>
+        <translation>Pixelclock</translation>
+    </message>
+    <message>
+        <location filename="../src/widgetdock_camera.cpp" line="53"/>
+        <source>Framerate</source>
+        <translation>Framerate</translation>
+    </message>
+    <message>
+        <location filename="../src/widgetdock_camera.cpp" line="54"/>
+        <source>Exposure</source>
+        <translation>Belichtungszeit</translation>
+    </message>
+    <message>
+        <location filename="../src/widgetdock_camera.cpp" line="55"/>
+        <source>Gain</source>
+        <translation>Gain</translation>
+    </message>
+    <message>
+        <location filename="../src/widgetdock_camera.cpp" line="56"/>
+        <source>Gain boost</source>
+        <translation>Gain Boost</translation>
+    </message>
+    <message>
+        <location filename="../src/widgetdock_camera.cpp" line="57"/>
+        <source>Default light</source>
+        <translation>Standardhelligkeit</translation>
+    </message>
+    <message>
+        <location filename="../src/widgetdock_camera.cpp" line="58"/>
+        <source>Min light</source>
+        <translation>Minimale Helligkeit</translation>
+    </message>
+    <message>
+        <location filename="../src/widgetdock_camera.cpp" line="59"/>
+        <source>Max light</source>
+        <translation>Maximale Helligkeit</translation>
+    </message>
+    <message>
+        <location filename="../src/widgetdock_camera.cpp" line="61"/>
+        <source> MHz</source>
+        <translation> MHz</translation>
+    </message>
+    <message>
+        <location filename="../src/widgetdock_camera.cpp" line="62"/>
+        <source> fps</source>
+        <translation> fps</translation>
+    </message>
+    <message>
+        <location filename="../src/widgetdock_camera.cpp" line="63"/>
+        <source> ms</source>
+        <translation> ms</translation>
+    </message>
+    <message>
+        <location filename="../src/widgetdock_camera.cpp" line="289"/>
+        <location filename="../src/widgetdock_camera.cpp" line="290"/>
+        <source>%1 MHz</source>
+        <translation>%1 MHz</translation>
+    </message>
+    <message>
+        <location filename="../src/widgetdock_camera.cpp" line="291"/>
+        <source>(%1 MHz)</source>
+        <translation>(%1 MHz)</translation>
+    </message>
+    <message>
+        <location filename="../src/widgetdock_camera.cpp" line="326"/>
+        <location filename="../src/widgetdock_camera.cpp" line="327"/>
+        <source>%1 fps</source>
+        <translation>%1 fps</translation>
+    </message>
+    <message>
+        <location filename="../src/widgetdock_camera.cpp" line="328"/>
+        <source>(%1 fps)</source>
+        <translation>(%1 fps)</translation>
+    </message>
+    <message>
+        <location filename="../src/widgetdock_camera.cpp" line="363"/>
+        <location filename="../src/widgetdock_camera.cpp" line="364"/>
+        <source>%1 ms</source>
+        <translation>%1 ms</translation>
+    </message>
+    <message>
+        <location filename="../src/widgetdock_camera.cpp" line="365"/>
+        <source>(%1 ms)</source>
+        <translation>(%1 ms)</translation>
+    </message>
+    <message>
+        <location filename="../src/widgetdock_camera.cpp" line="388"/>
+        <location filename="../src/widgetdock_camera.cpp" line="389"/>
+        <source>%1%</source>
+        <translation>%1%</translation>
+    </message>
+    <message>
+        <location filename="../src/widgetdock_camera.cpp" line="390"/>
+        <source>(%1%)</source>
+        <translation>(%1%)</translation>
+    </message>
+</context>
+<context>
+    <name>linescan::widgetdock_mcl</name>
+    <message>
+        <location filename="../src/widgetdock_mcl.cpp" line="18"/>
+        <source>MCL</source>
+        <translation>MCL</translation>
+    </message>
+    <message>
+        <location filename="../src/widgetdock_mcl.cpp" line="20"/>
+        <source>Joystick</source>
+        <translation>Joysick</translation>
+    </message>
+    <message>
+        <location filename="../src/widgetdock_mcl.cpp" line="21"/>
+        <source>Reset position</source>
+        <translation>Position zurücksetzen</translation>
+    </message>
+    <message>
+        <location filename="../src/widgetdock_mcl.cpp" line="39"/>
+        <source>X: %1 mm</source>
+        <translation>X: %1 mm</translation>
+    </message>
+    <message>
+        <location filename="../src/widgetdock_mcl.cpp" line="40"/>
+        <source>Y: %1 mm</source>
+        <translation>Y: %1 mm</translation>
+    </message>
+    <message>
+        <location filename="../src/widgetdock_mcl.cpp" line="41"/>
+        <source>Z: %1 mm</source>
+        <translation>Z: %1 mm</translation>
+    </message>
+</context>
+</TS>
